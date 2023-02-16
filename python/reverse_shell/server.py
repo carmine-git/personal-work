7960188ae2aef556bbc6cc57760731b51858ee17
carmineamaryllis <amaryllismain09@gmail.com>
2023-02-16 10:49:14 +0100

    Feat: socket server first version

---
+++ python/reverse_shell/server.py
@@ -0,0 +1,24 @@
+from socket import AF_INET, SOCK_STREAM, socket, gethostname
+
+
+def server_connection_script():
+    host = gethostname()
+    port = 8080
+    server = socket()
+    server.bind((host, port))
+    server.listen(2)
+
+    with server.accept() as connection, server.accept() as address:
+        print(f"Connection: {str(address)}")
+
+        while True:
+            data = connection.recv(1024).decode()
+            if not data:
+                break
+            print("Connected user's data " + str(data))
+            data = input("  =>  ")
+            connection.send(data.encode())
+
+
+if __name__ == "__main__":
+    server_connection_script()
