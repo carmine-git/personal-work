e5ce454459ea64dcb4278bc343d8160707e845ae
carmineamaryllis <amaryllismain09@gmail.com>
2023-02-22 21:23:31 +0100

    Chore: add stack.c file

---
+++ clang/stack.c
@@ -0,0 +1,56 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack {
+  int value;
+  struct Stack *previous;
+};
+
+/** @Brief Pushes to the top of the stack
+ * @param stack the stack to modify
+ * @param value the value to push in the stack
+ * @return void
+ */
+void push(struct Stack **stack, int value) {
+  struct Stack *new_stack = (struct Stack *)malloc(sizeof(struct Stack *));
+
+  if (new_stack != NULL) {
+    new_stack->value = value;
+    new_stack->previous = (*stack);
+    (*stack) = new_stack;
+  }
+};
+
+/** @Brief displays the stack values
+ * @param stack the stack values to be displayed
+ * @return void
+ */
+void display(struct Stack *stack) {
+	printf("\n[");
+
+	while (stack != NULL) {
+		printf("%d, ", stack->value);
+		stack = stack->previous;
+	}
+
+	printf("]");
+}
+
+/** @brief Checks if the stack is empty
+ * @param stack the stack to check
+ * @return bool
+ */
+bool is_empty(struct Stack *stack) {
+	return stack == NULL;
+}
+
+int main() {
+  struct Stack *stack = NULL;
+  push(&stack, 3);
+	push(&stack, 45);
+	push(&stack, 86);
+	display(stack);
+  printf("%d", is_empty(stack));
+  return 0;
+}
